fe22b21fa3c84af56f94ec10f62c8a2cf96f4a91
Maksim Murygin <muriginm@gmail.com>
2016-04-14 22:43:46 +0600

    install middlewares

--- app.js
+++ app.js
@@ -1,12 +1,20 @@
-var express = require('express');
-var path = require('path');
-var favicon = require('serve-favicon');
-var logger = require('morgan');
-var cookieParser = require('cookie-parser');
-var bodyParser = require('body-parser');
+'use strict';
 
-var routes = require('./routes/index');
-var users = require('./routes/users');
+const express = require('express');
+const path = require('path');
+const logger = require('morgan');
+const cookieParser = require('cookie-parser');
+const bodyParser = require('body-parser');
+const session = require('express-session');
+const multer = require('multer');
+const flash = require('connect-flash');
+const mongo = require('mongodb');
+const expressValidator = require('express-validator');
+const expressMessages = require('express-messages');
+const db  = require('monk')('localhost/nodeblog');
+
+const routes = require('./routes/index');
+const users = require('./routes/users');
 
 var app = express();
 
@@ -14,13 +22,52 @@ var app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
+// Handle File Uploads
+app.use(multer({ dest: './uploads/images'}). any() );
+
 // uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+
+// Handle Express Sessions
+app.use(session({
+  secret: 'lsdfjasdf_%dfss',
+  saveUninitialized: true,
+  resave: true
+}));
+
+// Validator
+app.use(expressValidator({
+  errorFormatter: function(param, msg, value) {
+    var namespace = param.split('.');
+    var root    = namespace.shift();
+    var formParam = root;
+
+    while(namespace.length) {
+      formParam += '[' + namespace.shift() + ']';
+    }
+    return {
+      param : formParam,
+      msg   : msg,
+      value : value
+    };
+  }
+}));
+
+app.use(flash());
+app.use(function (req, res, next) {
+  res.locals.messages = expressMessages(req, res);
+  next();
+});
+
+// Make our db accessible to our router
+app.use(function (req, res, next) {
+  req.db = db;
+  next();
+});
 
 app.use('/', routes);
 app.use('/users', users);
