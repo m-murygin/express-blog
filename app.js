a54ad553715b37f23e81085c016371b378a821a1
Maksim Murygin <muriginm@gmail.com>
2016-04-17 23:26:12 +0600

    home view + possts controller

--- app.js
+++ app.js
@@ -14,9 +14,10 @@ const expressMessages = require('express-messages');
 const db  = require('monk')('localhost/nodeblog');
 
 const routes = require('./routes/index');
-const users = require('./routes/users');
+const posts = require('./routes/posts');
 
 var app = express();
+app.locals.moment = require('moment');
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -70,7 +71,7 @@ app.use(function (req, res, next) {
 });
 
 app.use('/', routes);
-app.use('/users', users);
+app.use('/posts', posts);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
@@ -84,7 +85,7 @@ app.use(function(req, res, next) {
 // development error handler
 // will print stacktrace
 if (app.get('env') === 'development') {
-  app.use(function(err, req, res, next) {
+  app.use(function(err, req, res) {
     res.status(err.status || 500);
     res.render('error', {
       message: err.message,
@@ -95,7 +96,7 @@ if (app.get('env') === 'development') {
 
 // production error handler
 // no stacktraces leaked to user
-app.use(function(err, req, res, next) {
+app.use(function(err, req, res) {
   res.status(err.status || 500);
   res.render('error', {
     message: err.message,
