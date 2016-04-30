ebdb352913580aeccef58f42d1ad5c32309e4f34
Maksim Murygin <muriginm@gmail.com>
2016-04-30 23:08:19 +0600

    implement post adding

--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,6 @@
 
 const express = require('express');
 const router = express.Router();
-const mongo = require('mongodb');
 
 /* GET home page. */
 router.get('/', function(req, res) {
--- routes/posts.js
+++ routes/posts.js
@@ -1,9 +1,72 @@
-var express = require('express');
-var router = express.Router();
+'use strict';
+
+const express = require('express');
+const router = express.Router();
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+router.get('/add', function(req, res) {
+  req.db
+    .get('categories')
+    .find({})
+    .then(cats => {
+      res.render('add_post', {
+        title: 'Add Post',
+        categories: cats,
+      });
+    });
+});
+
+router.post('/add', function (req, res) {
+  // get the form values
+  let title = req.body.post_title;
+  let category = req.body.category;
+  let body = req.body.body;
+  let author = req.body.author;
+  let date = new Date();
+  
+  if (req.files.mainimage) {
+    var mainImageOriginalName = req.files.mainimage.originName;
+    var mainImageName = req.files.mainimage.name;
+    var mainImageMime = req.files.mainimage.mimetype;
+    var mainImagePath = req.files.mainimage.path;
+    var mainImageExt = req.files.mainimage.extension;
+    var mainImageSize = req.files.mainimage.size;
+  } else {
+    var mainImageName = 'noimage.png';
+  }
+
+  req.checkBody('title', 'Title field is required').notEmpty();
+  req.checkBody('body', 'Body field is required');
+
+  let errors = req.validationErrors();
+  if (errors) {
+    res.render('add_post', {
+      errors: errors,
+      body: body,
+      post_title: title,
+      title: 'Add Post',
+    });
+
+    return;
+  }
+
+  let posts = req.db.get('posts');
+
+  posts.insert({
+    title: title,
+    category: category,
+    body: body,
+    author: author,
+    date: date,
+  }, function (err, post) {
+    if (err) {
+      res.send('There was an issue submitting the post');
+    } else {
+      req.flash('success', 'Post Submitted');
+      res.location('/');
+      res.redirect('/');
+    }
+  });
 });
 
 module.exports = router;
